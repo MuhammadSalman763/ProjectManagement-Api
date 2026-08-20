b2cc8f199d2e23a4e9536c52f087b3fa4783b183
Muhammad Salman <salmanfateh12@gmail.com>
2026-08-20 15:25:07 +0500

    Register profile model in Django admin

---
+++ accounts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'role',
+        'contact_number',
+        'profile_picture',
+    )
