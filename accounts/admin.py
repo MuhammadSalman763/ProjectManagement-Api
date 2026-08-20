5f5b4de91fbc6a83a26870fd39fa20a8ba9c9b7f
Muhammad Salman <salmanfateh12@gmail.com>
2026-08-20 17:41:29 +0500

    Ticket 4: Create Project API

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Project
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = (
-        'id',
         'user',
         'role',
         'contact_number',
-        'profile_picture',
+    )
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'start_date',
+        'end_date',
+    )
+
+    filter_horizontal = (
+        'team_members',
     )
--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 
 class Profile(models.Model):
-
     ROLE_CHOICES = [
         ('manager', 'Manager'),
         ('qa', 'QA'),
@@ -35,3 +34,26 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Project(models.Model):
+    title = models.CharField(
+        max_length=200
+    )
+
+    description = models.TextField(
+        blank=True
+    )
+
+    start_date = models.DateField()
+
+    end_date = models.DateField()
+
+    team_members = models.ManyToManyField(
+        User,
+        related_name='projects',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
